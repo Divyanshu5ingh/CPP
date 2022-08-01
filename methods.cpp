6fbb383e0027d2dcf6db4673e1cf827eadb8bcaf
Divyanshu Kumar <90168140+Divyanshu5ingh@users.noreply.github.com>
2022-08-02 00:20:09 +0530

    Add files via upload

---
+++ methods.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class MyClass {
+    public:
+    void myName() {
+        cout<<"My name is Divyanshu Kumar";
+    }
+};
+
+int main()
+{
+    MyClass myObj;
+    myObj.myName();
+
+    return 0;
+}
